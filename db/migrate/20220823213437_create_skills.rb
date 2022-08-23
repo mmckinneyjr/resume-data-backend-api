238c12c1e5c27d30ebb2aca94ea8b1c81d09d523
Mark Mckinney <mmckinneyjr@gmail.com>
2022-08-23 16:36:40 -0500

    add skills and capstone models

---
+++ db/migrate/20220823213437_create_skills.rb
@@ -0,0 +1,10 @@
+class CreateSkills < ActiveRecord::Migration[7.0]
+  def change
+    create_table :skills do |t|
+      t.integer :student_id
+      t.string :skill
+
+      t.timestamps
+    end
+  end
+end
