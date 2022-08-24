d33eee105c022369e0381be231a9e2afd1c7164a
CalebFrederick <caleb@heckfamily.com>
2022-08-24 13:01:03 -0400

    added educations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_23_215445) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_24_164941) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_23_215445) do
     t.text "description"
     t.string "url"
     t.string "screenshot"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "educations", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.string "degree"
+    t.string "university_name"
+    t.text "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
